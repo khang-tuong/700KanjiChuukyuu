114fd174b68a7db973781ba99b04adbe486e3e73
khang-tuong <jakelong1232007@gmail.com>
2016-11-15 10:25:28 +0700

    Create main form

--- 700KanjiChuukyuu/MainForm.cs
+++ 700KanjiChuukyuu/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace _700KanjiChuukyuu
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
